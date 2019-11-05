effb5cc5f932563c2558d14b7e4837ea92890c62
schu8762 <J.Schuller@ou.edu>
2019-11-05 11:43:30 -0600

    last

---
+++ Part 11-5/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_11_5
+{
+    class TextBox
+    {
+        public int Padding {get; set;}
+
+        public string Text { get; set; }
+            
+        public ConsoleColor BackColor { get; set; }
+
+        public ConsoleColor ForeColor { get; set; }
+
+        public TextBox()
+        {
+            Padding = 0;
+            Text = string.Empty;
+            BackColor = ConsoleColor.Black;
+            ForeColor = ConsoleColor.White;
+        }
+
+        public void DisplayText()
+        {
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            string sentence = $"{Text.PadLeft(Padding).PadRight(Padding)}";
+            Console.WriteLine(sentence);
+                      
+        }
+        
+
+
+
+
+    }
+}
--- Part 11-5/Program.cs
+++ Part 11-5/Program.cs
@@ -10,7 +10,33 @@ namespace Part_11_5
     {
         static void Main(string[] args)
         {
-           
+            TextBox tb = new TextBox();
+            TextBox tb2 = new TextBox();
+
+            tb.Text = "chicken";
+            tb.BackColor = ConsoleColor.Blue;
+            tb.ForeColor = ConsoleColor.Cyan;
+            tb.Padding = 4;
+
+            tb2.Text = "tea";
+            tb2.BackColor = ConsoleColor.Green;
+            tb2.ForeColor = ConsoleColor.Red;
+            tb2.Padding = 10;
+
+        
+            tb.DisplayText();
+            tb2.DisplayText();
+
+            Console.ReadKey();
+            
+
         }
+
+
+
+
+
+
+
     }
 }
